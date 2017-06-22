e11d78559b11641a20f820023692a5d26fee36c1
Cabeceiras <Cabeceiras@users.noreply.github.com>
2017-06-21 21:43:10 -0300

    Update 05deAbril_Exercicio_1_(0a10).py

--- 05deAbril_Exercicio_1_(0a10).py
+++ 05deAbril_Exercicio_1_(0a10).py
@@ -1,3 +1,8 @@
+'''
+1) Faça um programa que peça uma nota, entre zero e dez. 
+Mostre uma mensagem caso o valor seja inválido e continue pedindo
+até que o usuário informe um valor válido.
+'''
 numero=int(input("Insira número de 0 a 10: "))
 
 while numero<0 or numero>10:
