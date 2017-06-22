4096474f22f304605af67d00a8c19c8e367740d2
Cabeceiras <Cabeceiras@users.noreply.github.com>
2017-06-22 11:22:41 -0300

    Update 03deMaio_Desafio_3_(bissexto).py

--- 03deMaio_Desafio_3_(bissexto).py
+++ 03deMaio_Desafio_3_(bissexto).py
@@ -1,3 +1,12 @@
+'''
+3) - Determine se um ano é bissexto:para ser bissexto, o ano deve ser:
+Divisível  4. Sendo assim, a divisão é exata com o resto igual a zero; 
+Não pode ser divisível por 100. Com isso, a divisão não é exata, ou seja,
+deixa resto diferente de zero; Pode ser que seja divisível por 400. 
+Caso seja divisível por 400, a divisão deve ser exata, deixando o resto 
+igual a zero.
+'''
+
 opcao=1
 while opcao==1:
 
