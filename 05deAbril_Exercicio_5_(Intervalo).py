3e548574feb27bb83e9fdde8fe89277f74ba2993
Cabeceiras <Cabeceiras@users.noreply.github.com>
2017-06-21 21:45:13 -0300

    Update 05deAbril_Exercicio_5_(Intervalo).py

--- 05deAbril_Exercicio_5_(Intervalo).py
+++ 05deAbril_Exercicio_5_(Intervalo).py
@@ -1,3 +1,9 @@
+'''
+5) Faça um programa que receba dois números inteiros 
+e gere os números inteiros que estão no intervalo 
+compreendido por eles.
+'''
+
 inteiro1=int(input("Informe o primeiro número inteiro: "))
 inteiro2=int(input("Informe o segundo número inteiro: "))
 
