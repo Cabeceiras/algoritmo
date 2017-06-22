0316a153dfa14b05cf0cf6bdd921eaa432841f0b
Cabeceiras <Cabeceiras@users.noreply.github.com>
2017-06-22 13:17:20 -0300

    Update 24deMaio_Exercicio_0_(TanqueKmL).py

--- 24deMaio_Exercicio_0_(TanqueKmL).py
+++ 24deMaio_Exercicio_0_(TanqueKmL).py
@@ -1,3 +1,9 @@
+'''
+Exercício 0: Faça um programa onde o usuário informe a km no  momento que o tanque é cheio,
+no próximo abastecimento o usuário deve informar os km percorridos e a quantidade de combustível
+necessária para completar o tanque (l). O sistema deve informar a o consumo médio (km/l). 
+'''
+
 print()
 print("Atenção. Este programa, somente pode ser usado logo após o tanque ser abastecido.")
 print()
